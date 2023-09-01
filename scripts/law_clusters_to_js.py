981865e8e046b3c288c82d93c270989a1f88f4a4
Daniel Ohayon <7629631+daniel-ohayon@users.noreply.github.com>
2023-08-31 21:55:53 -0500

    update clusters data, add explanatory modal, add link highlighting

--- scripts/law_clusters_to_js.py
+++ scripts/law_clusters_to_js.py
@@ -1,10 +1,10 @@
 import csv
 import json
 
-with open('raw_data/law_clusters_annotated.csv', encoding='utf8') as f:
+with open('raw_data/law_clusters_annotated_2.csv', encoding='utf8') as f:
     with open('../data/laws/law-clusters.js', 'w') as out_file:
         all_rows = list(csv.DictReader(f))
-        out_file.write(f"const LAW_CLUSTERS = {json.dumps(all_rows, indent=2, ensure_ascii=False)};")
+        out_file.write(f"const LAWS_IN_CLUSTERS = {json.dumps(all_rows, indent=2, ensure_ascii=False)};")
 
 
 
