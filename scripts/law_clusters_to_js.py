ddea2d5de7ccc28f9f6544ad2a370b232882add5
Daniel Ohayon <7629631+daniel-ohayon@users.noreply.github.com>
2023-12-12 18:09:54 -0600

    update law clusters data

--- scripts/law_clusters_to_js.py
+++ scripts/law_clusters_to_js.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-with open('raw_data/law_clusters_edited_v4.csv', encoding='utf8') as f:
+with open('raw_data/law_clusters_edited_v5.csv', encoding='utf8') as f:
     with open('../data/laws/law-clusters.js', 'w') as out_file:
         all_rows = list(csv.DictReader(f))
         out_file.write(f"const LAWS_IN_CLUSTERS = {json.dumps(all_rows, indent=2, ensure_ascii=False)};")
